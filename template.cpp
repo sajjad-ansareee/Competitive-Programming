93c8da55f647ec7ecf8857d82190391820f2923b
sajjad-ansareee <sajjadansareee@gmail.com>
2026-07-01 11:39:22 +0500

    add template file

---
+++ template.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+
+}
+
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    long long t=1; cin>>t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
